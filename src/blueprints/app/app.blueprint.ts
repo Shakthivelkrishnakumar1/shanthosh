654436b67186a32ce2fc71a5fb4685bebe02e315
Shakthivel K <71762131046@cit.edu.in>
2024-08-07 14:07:50 +0530

    second

--- src/blueprints/app/app.blueprint.ts
+++ src/blueprints/app/app.blueprint.ts
@@ -4,4 +4,3 @@ export enum Abbreviations {
     login = "LGN",
     baseLayout = "BL"
 }
-
--- src/blueprints/login/login.blueprint.ts
+++ src/blueprints/login/login.blueprint.ts
@@ -1,5 +1,11 @@
 import { Abbreviations } from "../app/app.blueprint";
 
 export class LoginBluePrint {
-    public static _login
+    public static _loginPageBlueprint:any={
+        pageCode:Abbreviations.login,
+        sectionCode:"LGN_CARD",
+        sectionName: "card",
+        
+
+    }
 }
